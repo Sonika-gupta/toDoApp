007cec206d8d857488b37bbcc86ee6737b2f1f3f
SG <sonikagupta94@gmail.com>
2020-11-11 23:44:01 +0530

    Added task panel front-end

--- app2.js
+++ app2.js
@@ -1,10 +1,16 @@
-class task {
+const color = {
+    none: '',
+    low: '#3465a4',
+    medium: '#f57900',
+    high: '#cc0000'
+};
+class Task {
     constructor({id, title, notes, priority, deadline, isComplete}) {
         this.id = id
         this.title = title
         this.notes = notes || ''
-        this.priority = priority || 'none'
-        this.deadline = deadline || ''
+        this.priority = priority || 'low'
+        this.deadline = deadline || 'Today'
         this.isComplete = isComplete || false
     }
     updateTask(task) {
@@ -12,7 +18,7 @@ class task {
     }
 }
 
-class toDoApp {
+class ToDoApp {
     constructor() {
         this.list = [];
         this.nextId = 1;
@@ -26,6 +32,7 @@ class toDoApp {
     }
     addTask(task) {
         task.id = this.nextId;
+        task = new Task(task)
         this.nextId++;
         this.list.push(task);
         this.setList(this.list)
@@ -43,7 +50,7 @@ class toDoApp {
     }
 }
 
-var app = new toDoApp();
+var app = new ToDoApp();
 
 function createItem (type, props, ...children) {
     let item = document.createElement(type);
@@ -54,26 +61,40 @@ function createItem (type, props, ...children) {
     }
     return item;
 }
+function expandPanel() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "grid")
+        panel.style.display = "none";
+    else
+        panel.style.display = "grid";
+}
 
 function appendData (data) {
     const newTask = app.addTask({title: data})
-
+    console.log(newTask);
     const menu = createItem('div', {className: 'icon'}, createItem('img', {className: 'small', src: 'menu.png'}));
     const checkbox = createItem('input', {type: 'checkbox', className: 'icon', id: newTask.id, value: false});
-    const title = createItem('label', {htmlFor: newTask.id}, createItem('div', {className: 'text'}, data));
-    const date = createItem('div', {className: 'date'})
-    const expand = createItem('div', {className: 'icon'}, createItem('img', {src: 'down.png'}));
-
+    const title = createItem('input', {className: 'text', value: data});
+    const date = createItem('div', {className: 'date'}, newTask.deadline)
+    const expand = createItem('div', {className: 'icon expand'}, createItem('img', {src: 'down.png'}));
     
-    const notes = createItem('div',  {className: 'notes'}, createItem('label', {}, 'Notes', createItem('input', {type: 'textarea', value: newTask.notes})))
-    const deadline = createItem('div',  {className: 'deadline'}, createItem('label', {}, 'Due Date', createItem('input', {type: 'date', value: newTask.deadline})))
-    const priority = createItem('div',  {className: 'priority'}, createItem('label', {}, 'Priority', createItem('input', {type: 'select', value: newTask.priority})))
-    const deleteButton = createItem('input', {className: 'deleteButton', type: 'button'}, 'Delete')
+    const notes = createItem('div',  {className: 'notes'}, 
+        createItem('label',{for: 'notes'}, 'Notes'),
+        createItem('textarea', {className: 'bordered', name: 'notes', value: newTask.notes}))
+    const deadline = createItem('div',  {className: 'deadline'},
+        createItem('label', {for: 'deadline'}, 'Due Date'),
+        createItem('input', {type: 'date', className: 'bordered', name: 'deadline', value: newTask.deadline}))
+    const priority = createItem('div',  {className: 'priority'},
+        createItem('label', {for: 'priority'}, 'Priority'),
+        createItem('input', {type: 'select', className: 'bordered', name: 'priority', value: newTask.priority}))
+    // const deleteButton = createItem('div', {className: 'deleteButton'}, createItem('button', {for: 'notes'}, 'Delete'))
+    const deleteButton = createItem('button', {className: 'bordered deleteButton'}, 'Delete')
 
     const container = createItem('div', {className: 'bordered task-container'},
-        createItem('div', {className: 'title-bar'}, menu, checkbox, title, date, expand),
+        createItem('div', {className: 'title-bar', onclick: expandPanel}, menu, checkbox, title, date, expand),
         createItem('div', {className: 'panel'}, notes, deadline, priority, deleteButton))
-
+    container.style.borderLeft = `5px solid ${color[newTask.priority]}`;
     document.getElementById('list').appendChild(document.createElement('li').appendChild(container))
 }
 function newItem () {
